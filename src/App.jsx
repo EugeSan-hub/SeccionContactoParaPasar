f2c7b6163fcb899402aba618ea184522944c9cc0
Eugenia Sansierra <eugesans@gmail.com>
2024-10-21 15:46:53 -0300

    se realizo seccion contacto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import SeccionContacto from './pages/SeccionContacto.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+function App() {
+  
+  return (
+    <>
+     <SeccionContacto></SeccionContacto>
+    </>
+  )
+}
+
+export default App
